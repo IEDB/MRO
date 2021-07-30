0ea37fb8769761a351a1774a57156c81c0b4637f
Apurva Mody <28663318+apmody@users.noreply.github.com>
2021-07-29 23:35:33 -0700

    Added Excluded Genes and logging messages

--- src/scripts/alleles/G_domain.py
+++ src/scripts/alleles/G_domain.py
@@ -1,12 +1,46 @@
-import Bio
-from Bio import SeqIO
+from Bio import SeqIO, BiopythonWarning
 import csv
 import sys
 import os
 sys.path.append(os.getcwd())
 from io import StringIO
-from Bio import BiopythonWarning
-
+import logging
+EXCLUDED_GENES = {
+    "BTN3A",
+    "CD1",
+    "DOA",
+    "DOB",
+    "V",
+    "P",
+    "S",
+    "Y",
+    "MICA",
+    "MICB",
+    "MR1",
+    "HFE",
+    "J",
+    "L",
+    "TAP1",
+    "DMB",
+    "DMA",
+    "T",
+    "V",
+    "W",
+    "K",
+    "TAP2",
+    "U",
+    "H",
+    "DRB8",
+    "DRB9",
+    "DRB7",
+    "DRB6",
+    "DPB2",
+    "DPA2",
+    "DQA2",
+    "DRB2"
+}
+logging.captureWarnings(True)
+logging.basicConfig(filename = "biopython.log", filemode = 'w', level = logging.INFO)
 
 chain_sequence = open("ontology/chain-sequence.tsv", "r")
 
@@ -20,9 +54,17 @@ excluded_sequence = []
 
 G_domains = []
 for entry in SeqIO.parse("build/hla.dat", "imgt" ):
+    if entry.name == 'HLA06639':
+        import pdb; pdb.set_trace()
+    # if entry.name == 'HLA06674':
+    #     import pdb; pdb.set_trace()
+    logging.info(entry.name + " beginning")
     has_exon_1 = False
+    logging.info(entry.name + " beginning 1")
     if entry.name not in acc or entry.description.split(",")[0].endswith("N"):
+        logging.info(entry.name + " endswith N")
         continue
+    logging.info(entry.name + " description")
     exons = []
     cds = ""
     exons_to_get = []
@@ -39,17 +81,25 @@ for entry in SeqIO.parse("build/hla.dat", "imgt" ):
     else:
         exons_to_get = ['2']
         type = 4
+    logging.info(entry.name + " type")
     G_domain_nuc = ""
     G_domain = ""
+    logging.info("Initialized everything")
     try:
         for feature in entry.features:
             if 'number' in feature.qualifiers and feature.qualifiers['number'][0] == '1' and feature.type == 'exon':
                 has_exon_1 = True
                 exons.append(feature)
+                logging.info("appended")
             if 'number' in feature.qualifiers and (feature.qualifiers['number'][0] in exons_to_get) and feature.type == 'exon':
                 exons.append(feature)
+                logging.info("appended")
             if feature.type == 'CDS':
                 cds = feature
+                logging.info("cds")
+                if cds.qualifiers['gene'][0] in EXCLUDED_GENES:
+                    logging.info("excluded gene")
+                    continue
             if len(exons) == 3:
                 break
         codon_start = int(cds.qualifiers['codon_start'][0])
@@ -103,22 +153,25 @@ for entry in SeqIO.parse("build/hla.dat", "imgt" ):
             excluded_sequence.append(entry.name)
             continue
         if not G_domain:
+            if len(G_domain_nuc) % 3 != 0:
+                #logging.debug(G_domain_nuc)
+                G_domain_nuc = G_domain_nuc + ('N' * (3 - len(G_domain_nuc) % 3))
             G_domain = G_domain_nuc.translate()
+            if G_domain[-1] == 'X':
+                G_domain = G_domain[:-1]
         if 'seq' in dir(G_domain) and str(G_domain.seq) not in cds.qualifiers['translation'][0]:
             import pdb; pdb.set_trace()
-            print(entry.name, G_domain.seq, cds.qualifiers['translation'][0], "not matched" )
+            logging.error(msg = f"{entry.name}, {str(G_domain.seq)}, {cds.qualifiers['translation'][0]} not matched" )
         if 'seq' in dir(G_domain):
             G_domain =  str(G_domain.seq)
         else:
             G_domain = G_domain
         G_domains.append({"Label": data[entry.name], "minimal HLA G domain sequence" : G_domain})
-    except BiopythonWarning:
-        print("BiopythonWarning")
     except AttributeError:
         if str(entry.seq) == 'X':
             excluded_sequence.append(entry)
-
-print(excluded_sequence)
+    logging.info(entry.name + " ending")
+logging.error(excluded_sequence)
 
 import csv
 with open("ontology/G-domain-sequence.tsv", "w") as fh:
