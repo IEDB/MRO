974f6b733a6da8c2a0d40c6505e8cc1f73b1258c
Rebecca Jackson <rbca.jackson@gmail.com>
2022-01-11 10:42:20 -0800

    Add MRO IDs to chains

--- src/scripts/add_chain_accessions.py
+++ src/scripts/add_chain_accessions.py
@@ -5,6 +5,7 @@ from argparse import ArgumentParser, FileType
 
 def main():
     parser = ArgumentParser()
+    parser.add_argument("index", type=FileType("r"))
     parser.add_argument("iedb", type=FileType("r"))
     parser.add_argument("molecule", type=FileType("r"))
     parser.add_argument("chain_sequence", type=FileType("r"))
@@ -19,31 +20,47 @@ def main():
             continue
         chain_accessions[row["Label"]] = row["Accession"]
 
+    label_to_id = {}
+    reader = csv.DictReader(args.index, delimiter="\t")
+    next(reader)
+    for row in reader:
+        if row["Label"] not in chain_accessions:
+            continue
+        label_to_id[row["Label"]] = row["ID"]
+
     molecules = {}
     reader = csv.DictReader(args.molecule, delimiter="\t")
     next(reader)
     for row in reader:
         chain_i_label = row["Alpha Chain"]
         chain_i_acc = None
+        chain_i_id = None
         if chain_i_label:
             chain_i_acc = chain_accessions.get(chain_i_label)
+            chain_i_id = label_to_id.get(chain_i_label)
         chain_ii_label = row["Beta Chain"]
         chain_ii_acc = None
+        chain_ii_id = None
         if chain_ii_label:
             chain_ii_acc = chain_accessions.get(chain_ii_label)
-        molecules[row["Label"]] = {"chain_i": chain_i_acc, "chain_ii": chain_ii_acc}
+            chain_ii_id = label_to_id.get(chain_ii_label)
+        molecules[row["Label"]] = {"chain_i_acc": chain_i_acc, "chain_i_id": chain_i_id, "chain_ii_acc": chain_ii_acc, "chain_ii_id": chain_ii_id}
 
     rows = []
     reader = csv.DictReader(args.iedb, delimiter="\t")
     row = next(reader)
     row["Chain I Accession"] = "A MRO:has-chain-i-accession"
+    row["Chain I MRO ID"] = "A MRO:has-chain-i-id"
     row["Chain II Accession"] = "A MRO:has-chain-ii-accession"
+    row["Chain II MRO ID"] = "A MRO:has-chain-ii-id"
     rows.append(row)
     for row in reader:
         label = row["Label"]
         if label in molecules:
-            row["Chain I Accession"] = molecules[label]["chain_i"]
-            row["Chain II Accession"] = molecules[label]["chain_ii"]
+            row["Chain I Accession"] = molecules[label]["chain_i_acc"]
+            row["Chain I MRO ID"] = molecules[label]["chain_i_id"]
+            row["Chain II Accession"] = molecules[label]["chain_ii_acc"]
+            row["Chain II MRO ID"] = molecules[label]["chain_ii_id"]
         rows.append(row)
 
     writer = csv.DictWriter(
@@ -54,8 +71,10 @@ def main():
             "Locus",
             "Chain I Source ID",
             "Chain I Accession",
+            "Chain I MRO ID",
             "Chain II Source ID",
             "Chain II Accession",
+            "Chain II MRO ID",
         ],
         delimiter="\t",
         lineterminator="\n",
